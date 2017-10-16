251b2c6be331330e64d59e8c6e19c50bd42db4d8
Thomas Gambet <thomas.gambet@gmail.com>
2017-10-16 10:49:25 +0200

    load contracts on home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+  contracts: contract[] = [];
 
   ngOnInit() {
+    this.http.get('https://api.jcdecaux.com/vls/v1/contracts?apiKey=8d872049485a3cd80d956d3f53aaeda5427d47e4').subscribe(data => {
+      let results = []
+      for (let i in data) {
+        results.push(data[i]);
+      }
+      this.contracts = _.sortBy(_.filter(results, {country_code: "FR"}), ["name"]);
+    });
   }
 
+}
+
+interface contract {
+  name: string,
+  cities : string[],
+  commercial_name: string,
+  country_code: string
 }
