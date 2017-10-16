eb8f48008f739a9a67b5bc2f991b5fd8c0f05fd0
Thomas Gambet <thomas.gambet@gmail.com>
2017-10-16 11:29:07 +0200

    redirect on city selection

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import * as _ from 'lodash';
 
@@ -11,18 +12,25 @@ export class HomeComponent implements OnInit {
 
   constructor(
     private http: HttpClient,
+    private router: Router,
   ) { }
 
   contracts: contract[] = [];
 
   ngOnInit() {
-    this.http.get('https://api.jcdecaux.com/vls/v1/contracts?apiKey=8d872049485a3cd80d956d3f53aaeda5427d47e4').subscribe(data => {
-      let results = []
-      for (let i in data) {
-        results.push(data[i]);
-      }
-      this.contracts = _.sortBy(_.filter(results, {country_code: "FR"}), ["name"]);
-    });
+    if (this.contracts.length == 0) {
+      this.http.get('https://api.jcdecaux.com/vls/v1/contracts?apiKey=8d872049485a3cd80d956d3f53aaeda5427d47e4').subscribe(data => {
+        let results = []
+        for (let i in data) {
+          results.push(data[i]);
+        }
+        this.contracts = _.sortBy(_.filter(results, {country_code: "FR"}), ["name"]);
+      });
+    }
+  }
+
+  onChange(name: string) {
+    this.router.navigate(['/' + name.toLowerCase()]);
   }
 
 }
