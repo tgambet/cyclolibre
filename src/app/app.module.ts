0aaab9fef30ae0ab596ea2b2b513927bf1370f50
Thomas Gambet <thomas.gambet@gmail.com>
2017-10-19 13:10:06 +0200

    removed the settings component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,12 +18,10 @@ import { JcDecauxService } from './services/jc-decaux.service';
 import { ContractComponent } from './contract/contract.component';
 import { ContractResolverService } from './contract/contract-resolver.service';
 import { TableComponent } from './table/table.component';
-import { SettingsComponent } from './settings/settings.component';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
-  { path: 'settings', component: SettingsComponent },
   {
     path: ':id',
     component: ContractComponent,
@@ -56,7 +54,6 @@ const appRoutes: Routes = [
     MapComponent,
     ContractComponent,
     TableComponent,
-    SettingsComponent,
   ],
   imports: [
     BrowserModule,
