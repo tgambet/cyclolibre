a7d5c82d34c94eea01dd5d3c5fa3a061fa76408e
Thomas Gambet <thomas.gambet@gmail.com>
2017-10-21 05:55:23 +0200

    added missing jcDecauxApiKey to environment.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  gmApiKey: 'AIzaSyDT_R2S3ov_htZic2ZBPOcMF2ogO7U4mf4'
+  gmApiKey: 'AIzaSyDT_R2S3ov_htZic2ZBPOcMF2ogO7U4mf4',
+  jcDecauxApiKey: '8d872049485a3cd80d956d3f53aaeda5427d47e4'
 };
