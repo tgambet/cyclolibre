03c5b621c92d7fbd88f5110ca819fb6d2326da1f
Thomas Gambet <thomas.gambet@gmail.com>
2017-10-29 10:50:48 +0100

    cleaning

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: false,
-  gmApiKey: 'AIzaSyCFi9RePFTxmDzbOrrF60_gbX1xS7MYEU8',
-  jcDecauxApiKey: '8d872049485a3cd80d956d3f53aaeda5427d47e4'
+  gmApiKey: 'AIzaSyCFi9RePFTxmDzbOrrF60_gbX1xS7MYEU8'
 };
